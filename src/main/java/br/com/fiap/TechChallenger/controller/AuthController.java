32ad8b2214f26098b51720f7afdde3ea961f71e8
talita <talita.primo@luizalabs.com>
2025-04-23 17:41:03 -0300

    chore: code review

--- src/main/java/br/com/fiap/TechChallenger/controller/AuthController.java
+++ src/main/java/br/com/fiap/TechChallenger/controller/AuthController.java
@@ -1,16 +1,10 @@
 package br.com.fiap.TechChallenger.controller;
 
-import br.com.fiap.TechChallenger.dto.JwtResponse;
 import br.com.fiap.TechChallenger.dto.LoginRequest;
-import br.com.fiap.TechChallenger.security.JwtUtils;
-import br.com.fiap.TechChallenger.security.UserDetailsImpl;
+import br.com.fiap.TechChallenger.service.AutenticacaoUsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,24 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtils jwtUtils;
+    private final AutenticacaoUsuarioService autenticacaoUsuarioService;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        final Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getLogin(), loginRequest.getSenha()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        final String jwt = jwtUtils.generateJwtToken(authentication);
-
-        final UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
-        return ResponseEntity.ok(new JwtResponse(jwt,
-                userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                userDetails.getTipoUsuario()));
+        return autenticacaoUsuarioService.autenticar(loginRequest);
     }
 
 }
--- src/main/java/br/com/fiap/TechChallenger/entity/Usuario.java
+++ src/main/java/br/com/fiap/TechChallenger/entity/Usuario.java
@@ -7,11 +7,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Data
 public class Usuario {
 
     @Id
