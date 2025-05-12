df9c429f97696d263ee95da620d094c91a88785c
pativilaka <p.vilaka@gmail.com>
2025-05-11 22:23:25 -0300

    feat: cria validação nos DTOs com Bean Validation

--- src/main/java/br/com/fiap/TechChallenger/dto/UsuarioEditDTO.java
+++ src/main/java/br/com/fiap/TechChallenger/dto/UsuarioEditDTO.java
@@ -1,6 +1,10 @@
 package br.com.fiap.TechChallenger.dto;
 
 import br.com.fiap.TechChallenger.model.TipoUsuario;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -8,10 +12,21 @@ import java.util.List;
 @Data
 public class UsuarioEditDTO {
 
+    @NotBlank(message = "O nome é obrigatório.")
     private String nome;
+
+    @NotBlank(message = "O e-mail é obrigatório.")
+    @Email(message = "O e-mail deve ser válido.")
     private String email;
+
+    @NotBlank(message = "O login é obrigatório.")
     private String login;
+
+    @Valid
+    @NotNull(message = "O endereço é obrigatório.")
     private List<EnderecoDTO> endereco;
+
+    @NotNull(message = "O tipo de usuário é obrigatório.")
     private TipoUsuario tipoUsuario;
 
 }
