182b6d07631e2e4308a5a0666685b03bec4d1d28
Lucas Lanfredi <RM364773@fiap.com.br>
2025-07-05 09:49:07 -0300

    Correção docker file e ExceptionHandler

--- src/main/java/br/com/fiap/TechChallenger/gateways/controller/ControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/TechChallenger/gateways/controller/ControllerExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.Instant;
 
 @ControllerAdvice
@@ -27,4 +28,30 @@ public class ControllerExceptionHandler {
         }
         return ResponseEntity.status(status).body(erro);
     }
+
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class )
+    public ResponseEntity<ErroCustomizado> handleJdbcConstraintViolation(
+            SQLIntegrityConstraintViolationException ex,
+            HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ErroCustomizado erro = new ErroCustomizado(
+                Instant.now(),
+                status.value(),
+                "Violação de integridade",
+                request.getRequestURI()
+        );
+
+        if (ex.getMessage().contains("EMAIL")) {
+            erro.addErroValidacao("email", "Já existe um usuário cadastrado com este e-mail.");
+        } else if (ex.getMessage().contains("LOGIN")) {
+            erro.addErroValidacao("login", "Já existe um usuário cadastrado com este login.");
+        } else {
+            erro.addErroValidacao("dados", "Violação de restrição de integridade.");
+        }
+
+        return ResponseEntity.status(status).body(erro);
+    }
+ 
 }
